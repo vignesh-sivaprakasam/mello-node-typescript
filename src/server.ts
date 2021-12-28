9bdc6ae1e42c7974d99caef06bb156d9869deac4
Vignesh M <vm@twilio.com>
2021-12-28 10:58:13 +0530

    backend server init code

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+
+import logging from "./config/logging";
+import config from "./config/config";
+
+const NAMESPACE = "Server";
+
+const router = express();
+
+/* Logging the request */
+
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}, URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}, URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
+    });
+    next();
+});
+
+/* Parse the request */
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+/* Rules for API */
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000','http://mello-server.herokuapp.com','https://mello-server.herokuapp.com','http://mello-client.herokuapp.com', 'https://mello-client.herokuapp.com', 'http://mello-react.herokuapp.com','https://mello-react.herokuapp.com'];
+router.use(function(req, res, next) {
+
+        const origin = req.headers.origin || '';
+        if(allowedOrigins.indexOf(origin) > -1){
+                res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+        next();
+});
+
+/* Error Handling */
+router.use((req, res, next) => {
+    const error = new Error('not found');
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/* Create the server */
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
